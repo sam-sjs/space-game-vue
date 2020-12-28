f5b4c6a08596be06a1806cf0a82119c933ed1bb9
Sam Skinner <sam.sjskinner@gmail.com>
2020-12-28 22:36:17 +1100

    Finished templating NavLinks and adding first attempt at props

--- src/components/CenterConsole.vue
+++ src/components/CenterConsole.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="shipContainer">
-    <NavLinks/>
-    <NavLinks/>
-    <NavLinks/>
-    <NavLinks/>
+    <NavLinks direction="up" :position="sysBelow" prevLoc="sys_below_id"/>
+    <NavLinks direction="left" :position="sysRight" prevLoc="sys_right_id"/>
+    <NavLinks direction="right" :position="sysLeft" prevLoc="sys_left_id"/>
+    <NavLinks direction="down" :position="sysAbove" prevLoc="sys_above_id"/>
     <img src="@/assets/images/nav-dial.png" alt="Navigation Controls">
     <img src="@/assets/images/ship.png" class="ship" alt="Navigation Controls">
   </div>
@@ -14,6 +14,14 @@ import NavLinks from './NavLinks.vue'
 
 export default {
   name: 'CenterConsole',
+  data: function() {
+    return {
+      sysBelow: 'TOP',
+      sysRight: 'LEFT',
+      sysLeft: 'RIGHT',
+      sysAbove: 'BOTTOM'
+    };
+  },
   components: {
     NavLinks
   }
--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>Hi</div>
+  <div @click="navClick(prevLoc)" class="navLinks" :id="direction+'Nav'"></div>
 </template>
 
 <script>
 export default {
-  name: 'NavLinks'
+  name: 'NavLinks',
+  props: {
+    position: String,
+    direction: String,
+    prevLoc: String
+  },
+  methods: {
+    navClick: function(prevLoc) {
+      console.log(prevLoc);
+    }
+  }
 }
 </script>
 
 <style scoped>
+.navLinks {
+  position:absolute;
+  display: block;
+  text-align: center;
+  cursor: pointer;
+}
+
+#upNav {
+  top: 0px;
+  left: 50px;
+  height: 50px;
+  width: 150px;
+}
+
+#downNav {
+  bottom: 0px;
+  left: 50px;
+  height: 50px;
+  width: 150px;
+}
+
+#leftNav {
+  left: 0px;
+  top: 50px;
+  height: 150px;
+  width: 50px;
+}
+
+#rightNav {
+  right: 0px;
+  top: 50px;
+  height: 150px;
+  width: 50px;
+}
 </style>
